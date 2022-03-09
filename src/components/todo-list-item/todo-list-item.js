cabc3baf5fe1673f10c16d9865de2c6c328483e3
Volodymyr Korzhevskyi <korzhevskyiv@gmail.com>
2022-03-09 10:57:02 +0100

    realise important button

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -5,6 +5,7 @@ import "./todo-list-item.css";
 export default class TodoListItem extends Component {
   state = {
     done: false,
+    important: false,
   };
   onLabelClick = () => {
     this.setState({
@@ -12,9 +13,15 @@ export default class TodoListItem extends Component {
     });
   };
 
+  onMarkImportant = () => {
+    this.setState({
+      important: true,
+    })
+  }
+  
   render() {
-    const { label, important = false } = this.props;
-    const { done } = this.state;
+    const { label } = this.props;
+    const { done, important } = this.state;
     const style = {
       color: important ? "steelblue" : "black",
       fontWeight: important ? "bold" : "normal",
@@ -23,6 +30,10 @@ export default class TodoListItem extends Component {
     let classNames = "todo-list-item";
     if (done) {
       classNames += " done";
+    }
+
+    if (important) {
+      classNames += ' important';
     }
 
     return (
@@ -34,6 +45,7 @@ export default class TodoListItem extends Component {
         <button
           type="button"
           className="btn btn-outline-success btn-sm float-right"
+          onClick={this.onMarkImportant}
         >
           <i className="fa fa-exclamation" />
         </button>
