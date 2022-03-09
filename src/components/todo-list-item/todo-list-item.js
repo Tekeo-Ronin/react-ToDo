b25733359a06983bcff0d7459d909ea41cae0bc0
Volodymyr Korzhevskyi <korzhevskyiv@gmail.com>
2022-03-09 10:14:03 +0100

    realize done tasks

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,20 +1,33 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 import "./todo-list-item.css";
 
 export default class TodoListItem extends Component {
+  state = {
+    done: false,
+  };
+  onLabelClick = () => {
+    this.setState({
+      done: true,
+    });
+  };
+
   render() {
-
     const { label, important = false } = this.props;
-
+    const { done } = this.state;
     const style = {
       color: important ? "steelblue" : "black",
       fontWeight: important ? "bold" : "normal",
     };
 
+    let classNames = "todo-list-item";
+    if (done) {
+      classNames += " done";
+    }
+
     return (
       <span className="todo-list-item">
-        <span className="todo-list-item-label" style={style}>
+        <span className={classNames} style={style} onClick={this.onLabelClick}>
           {label}
         </span>
 
