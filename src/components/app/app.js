6779178ad888731b0bafcde96d109e2b80dfccf2
Volodymyr Korzhevskyi <korzhevskyiv@gmail.com>
2022-03-13 12:21:40 +0100

    realise active filter

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -18,6 +18,7 @@ export default class App extends Component {
       this.createTodoItem("Have a lunch"),
     ],
     term: "",
+    filter: "all", //active/all/done
   };
 
   createTodoItem(label) {
@@ -79,7 +80,7 @@ export default class App extends Component {
   };
 
   onSearchChange = (term) => {
-    this.setState({term});
+    this.setState({ term });
   };
 
   search(items, term) {
@@ -88,15 +89,31 @@ export default class App extends Component {
     }
 
     return items.filter((item) => {
-      return item.label.
-      toLowerCase().indexOf(term.toLowerCase()) > -1;
+      return item.label.toLowerCase().indexOf(term.toLowerCase()) > -1;
     });
   }
 
-  render() {
-    const { todoData, term } = this.state;
+  filter(items, filter) {
+    switch (filter) {
+      case "all":
+        return items;
+      case "active":
+        return items.filter((item) => !item.done);
+      case "done":
+        return items.filter((item) => item.done);
+      default:
+        return items;
+    }
+  }
 
-    const visibleItems = this.search(todoData, term);
+  onFilterChange = (filter) => {
+    this.setState({ filter });
+  };
+
+  render() {
+    const { todoData, term, filter } = this.state;
+
+    const visibleItems = this.filter(this.search(todoData, term), filter);
     const doneCount = todoData.filter((el) => el.done).length;
     const todoCount = todoData.length - doneCount;
 
@@ -105,7 +122,7 @@ export default class App extends Component {
         <AppHeader toDo={todoCount} done={doneCount} />
         <div className="top-panel d-flex">
           <SearchPanel onSearchChange={this.onSearchChange} />
-          <ItemStatusFilter />
+          <ItemStatusFilter filter={filter} onFilterChange={this.onFilterChange} />
         </div>
 
         <TodoList
